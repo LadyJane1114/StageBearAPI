bcd37ae18f151a62e2b84da25f7cf841ac2240bb
LadyJane1114 <sweet.jane.s@outlook.com>
2025-11-04 12:16:28 -0400

    New changes, finally

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ import http from 'http';
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html'});
-  res.write('Jane, this is your app. Drink some water. Now!');
+  res.write('New Changes');
   res.end();
 }).listen(3000);
