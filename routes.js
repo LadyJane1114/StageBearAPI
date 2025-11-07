f0c89bf9fad1115ff09ea1dbbedd6dd9974248e0
LadyJane1114 <sweet.jane.s@outlook.com>
2025-11-07 14:10:22 -0400

    updated routes page

--- routes.js
+++ routes.js
@@ -22,42 +22,47 @@ router.get('/', async (req, res) => {
     // make sure that any items are correctly URL encoded in the connection string (CODE FROM MSSQL)
         await sql.connect(dbConnectionString)
 
-        const result = await sql.query`SELECT * from [dbo].[Show] s
-                INNER JOIN [dbo].[Category] c
-                ON s.[CategoryID] = c.[CategoryID]
-                INNER JOIN [dbo].[Venue] v
-                ON s.[VenueID] = v.[VenueID]
-                INNER JOIN [dbo].[Owner] o
-                ON s.[OwnerID] = o.[OwnerID]
-                ORDER BY s.[Scheduled]`
-//NEED TO FIX THE ABOVE WITH THE ACTUAL DATA FROM THE APP'S TABLE - DO IT IN THE SQL THING IN AZURE, IT'LL BE EASIER THERE TO FIGURE OUT.
-        
-        // console.dir(result)
+        const result = await sql.query`SELECT s.[ShowID], s.[Title], s.[Description], s.[Scheduled], s.[DateRecorded], c.[CategoryID], c.[CategoryTitle], s.[Image], v.[VenueID], v.[VenueName], v.[StreetAddress], v.[City], v.[Region], v.[Country], v.[PostCode], v.[Phone] as VenuePhone, v.[VenueNotes], o.[OwnerID], o.[FName], o.[LName], o.[Pronouns], o.[Organization], o.[Phone] as OwnerPhone, o.[Email], o.[OwnerNotes] from [dbo].[Show] s
+            INNER JOIN [dbo].[Category] c
+            ON s.[CategoryID] = c.[CategoryID]
+            INNER JOIN [dbo].[Venue] v
+            ON s.[VenueID] = v.[VenueID]
+            INNER JOIN [dbo].[Owner] o
+            ON s.[OwnerID] = o.[OwnerID]
+            ORDER BY s.[Scheduled]`
 
     res.json(result.recordset);
 //    res.send('All shows');
 });
 
 //GET: /api/shows/:id
-router.get('/:id',(req, res) => {
+router.get('/:id', async (req, res) => {
+
     const id = req.params.id;
 
-    //sample show object
-    const show = {id: id, title: 'Sample Title', description: 'Sample description'};
+    if(isNaN(id)) {
+        res.status(400).send('Invalid show ID.');
+        return;
+    }
 
-    res.json(show)
+    await sql.connect(dbConnectionString)
 
-    // res.send(`Show details for show ID: ${id}`);
+        const result = await sql.query`SELECT s.[ShowID], s.[Title], s.[Description], s.[Scheduled], s.[DateRecorded], c.[CategoryID], c.[CategoryTitle], s.[Image], v.[VenueID], v.[VenueName], v.[StreetAddress], v.[City], v.[Region], v.[Country], v.[PostCode], v.[Phone] as VenuePhone, v.[VenueNotes], o.[OwnerID], o.[FName], o.[LName], o.[Pronouns], o.[Organization], o.[Phone] as OwnerPhone, o.[Email], o.[OwnerNotes] from [dbo].[Show] s
+            INNER JOIN [dbo].[Category] c
+            ON s.[CategoryID] = c.[CategoryID]
+            INNER JOIN [dbo].[Venue] v
+            ON s.[VenueID] = v.[VenueID]
+            INNER JOIN [dbo].[Owner] o
+            ON s.[OwnerID] = o.[OwnerID]
+            WHERE s.[ShowID]=${id}`
+
+    if(result.recordset.length === 0) {
+        res.status(404).send('Show not found.');
+    }
+    else {
+        res.json(result.recordset); 
+    }
+
 });
-
-// //GET: /api/shows/hello
-// router.get('/hello', (req, res) => {
-//   res.send('Hello Express!');
-// });
-
-// //GET: /api/shows/goodbye
-// router.get('/goodbye', (req, res) => {
-//   res.send('Goodbye!');
-// });
 
 export default router;
