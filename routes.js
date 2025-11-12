1176514102acc80d0b9035e5bdb971b74d64eb96
LadyJane1114 <sweet.jane.s@outlook.com>
2025-11-12 11:08:06 -0400

    standardized json

--- routes.js
+++ routes.js
@@ -13,13 +13,6 @@ const dbConnectionString = process.env.AZURE_DB_CONNECTION_STRING;
 
 //GET: /api/shows
 router.get('/', async (req, res) => {
-    // const shows = [
-    //     {id: 1, title: 'Thoroughly Modern Millie', description: 'A musical set in the 1920s', "category": 'musical'},
-    //     {id: 2, title: 'Annie', description: 'A musical about an orphan', "category": 'musical'},
-    //     {id: 3, title: 'Urinetown', description: 'It is a privilege to pee', "category": 'musical'}
-    // ]; SAMPLE CODE, BUT WE'RE NOT USING THAT
-
-    // make sure that any items are correctly URL encoded in the connection string (CODE FROM MSSQL)
         await sql.connect(dbConnectionString)
 
         const result = await sql.query`SELECT s.[ShowID], s.[Title], s.[Description], s.[Scheduled], s.[DateRecorded], c.[CategoryID], c.[CategoryTitle], s.[Image], v.[VenueID], v.[VenueName], v.[StreetAddress], v.[City], v.[Region], v.[Country], v.[PostCode], v.[Phone] as VenuePhone, v.[VenueNotes], o.[OwnerID], o.[FName], o.[LName], o.[Pronouns], o.[Organization], o.[Phone] as OwnerPhone, o.[Email], o.[OwnerNotes] from [dbo].[Show] s
@@ -41,7 +34,7 @@ router.get('/:id', async (req, res) => {
     const id = req.params.id;
 
     if(isNaN(id)) {
-        res.status(400).send('Invalid show ID.');
+        res.status(400).json({message:'Invalid show ID.'});
         return;
     }
 
@@ -57,7 +50,7 @@ router.get('/:id', async (req, res) => {
             WHERE s.[ShowID]=${id}`
 
     if(result.recordset.length === 0) {
-        res.status(404).send('Show not found.');
+        res.status(404).json({message : 'Show not found.'});
     }
     else {
         res.json(result.recordset); 
